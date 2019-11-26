5088010f198413d4fd54856e9bf3d7e5cde65c11
Pablo Miralles <pablomiralles99@gmail.com>
2019-11-26 18:27:15 +0100

    Fixed input treatment

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from sys import stderr
 from automatons.minimize import minimize
 
 if __name__ == '__main__':
-    path = input('Introduzca un autómata:')
-    try:
-        automaton = Afd(path)
-    except FileNotFoundError:
-        print('Me temo que la ruta está mal', file=stderr)
-    except Exception as error:
-        print('Problema analizando el fichero: ', error, file=stderr)
+    leer = True
+    while leer:
+        leer = False
+        path = input('Introduzca un autómata:')
+        if path == '':
+            print('No se debe introducir una ruta vacía.')
+            exit()
+        try:
+            automaton = Afd(path)
+        except FileNotFoundError:
+            print('Me temo que la ruta está mal.', file=stderr)
+            leer = True
+        except Exception as error:
+            print('Problema analizando el fichero: ', error, file=stderr)
     minimize(automaton)
